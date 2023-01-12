2cb83d7efc07732efeceab15ebb5d62fd517d733
VivekSGopalakrishnan <gv8464@srmist.edu.in>
2023-01-12 16:52:08 +0530

    updated db

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async()=>{
     try{
         mongoose.set('strictQuery', true);
-        const con = await mongoose.connect(process.env.MONGO_URI)
+        const con = await mongoose.connect(process.env.MONGO_URI , { useNewUrlParser: true, useUnifiedTopology: true })
 
         console.log(`Mongo Connected: ${con.connection.host}`);
     }
