2626ae1a7b84731ffc8b3956c2032929d5272517
VivekSGopalakrishnan <gv8464@srmist.edu.in>
2023-01-12 18:26:39 +0530

    added cors

--- index.js
+++ index.js
@@ -4,19 +4,28 @@ const port = process.env.PORT||5000
 const connectDB = require('./db')
 const name = require('./model')
 const app = express()
+const cors = require('cors')
+
+app.options("*", cors())
+
+const corsOptions = {
+  origin:"http://127.0.0.1:5173"
+}
+
+app.use(cors(corsOptions))
 
 connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-app.get('/api',async(req,res) => {
+app.get('/',async(req,res) => {
     const datas = await name.find()
     
     res.status(200).json( datas )
 })
 
-app.post('/api',async(req,res) => {
+app.post('/',async(req,res) => {
   if(!req.body.name){
     res.status(400)
     throw new Error('Please add a text')
@@ -34,4 +43,7 @@ app.post('/api',async(req,res) => {
   const val =await data1.save();
   res.status(200).json(data1)
 })
+
+
+
 app.listen(port, ()=> console.log(`server started on port ${port}`))
