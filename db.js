555ba5f3ec6c1ec68f65a618d5db4e69d4b0308f
VivekSGopalakrishnan <gv8464@srmist.edu.in>
2023-01-12 17:27:30 +0530

    updated link

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config()
 const connectDB = async()=>{
     try{
         mongoose.set('strictQuery', true);
-        const con = await mongoose.connect(process.env.MONGO_URI , { useNewUrlParser: true, useUnifiedTopology: true })
+        const con = await mongoose.connect("mongodb+srv://vivek:vivek014@cluster0.bxxikkg.mongodb.net/open-auto?retryWrites=true&w=majority" , { useNewUrlParser: true, useUnifiedTopology: true })
 
         console.log(`Mongo Connected: ${con.connection.host}`);
     }
